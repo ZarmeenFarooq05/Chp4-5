4193278a7df6f1953ea7f72a6024b8418df59ed5
ZarmeenFarooq05 <zfarooq2005@gmail.com>
2024-02-05 19:07:22 +0500

    first commit

---
+++ simple.js
@@ -0,0 +1,164 @@
+//Assignment 13/07/2023
+//Chapter: 4
+
+var a=b=c="apple"
+alert(a)
+alert(b)
+alert(c)
+
+//Legal
+var $y = 1+2
+var _u = "fed"
+var a_b = 56/3
+var q123 ="gtu"
+var AbC ="rdguk"
+
+//Ilegal
+
+// var 12 =""gfjgm";
+// var 'g' =34-6;
+// var prompt = "fyhj?"
+// var w a h = 9+0;
+// var 123& = ""fjhujjm";
+document.write("<h2>Chapter 4</h2>")
+document.write("<h1>Rules for naming variables in JS</h1>")
+document.write("<br>")
+document.write("Variable names can only contain numbers,$ and _ for example: $my_1stVariable")
+document.write("<br><br>")
+document.write("Variable must begin with a letter, $ or _ . For examplr: $name, _name or name")
+document.write("<br><br>")
+document.write("Variable names are case-sensitive")
+document.write("<br><br>")
+document.write("Variable names should not be JS keywords")
+document.write("<br><br>")
+document.write("<h2>Chapter 5</h2>")
+
+//Assignment 13/07/2023
+//Chapter: 5
+//Q3 = ?
+
+var sum = 3+5
+alert(sum)
+var sub = 3-5
+alert(sub)
+var mul = 3*5
+alert(mul)
+var divide = 3/5
+alert(divide)
+
+var ticket = 600
+tictotal = ticket*5
+alert(tictotal)
+
+var celsius = 28
+var ctof = (celsius*9/5)+32
+var feren = 95
+var ftoc = (feren-32)*5/9
+var value1 = 45
+var value1 = 56
+var ini = 70
+var item1 = 400
+var item2 = 900
+var quantity1 = 8
+var quantity2 = 7
+var ship = 100
+var totalc = item1*quantity1+item2*quantity2+ship
+var tmarks = 1100
+var omarks = 896
+var per = omarks*100/tmarks
+var dollar = 104.80
+var dq = 10
+var riyal = 28
+var rq = 25
+var pkr = dollar*dq+riyal*rq
+var sequence = 5*10/2
+var cyear = 2023
+var byear = 1992
+var age = cyear-byear
+var radius = 20
+var circum = 2*3.142*radius
+var area = (3.142)*2*radius
+var snack = "Lays"
+var day = 2
+var cage = 30
+var max = 85
+var tsnack = day*max-cage
+document.write(celsius +"<sup>o</sup>"+"C" +" " +"is" +" "+ ctof+"<sup>o</sup>"+"F")
+document.write("<br><br>")
+document.write(feren +"<sup>o</sup>" +"F" +"  " +"is" +" "+ftoc+"<sup>o</sup>"+"C")
+document.write("<h4>Table Of 4</h4>")
+document.write("4x1="+4*1)
+document.write("<br><br>")
+document.write("4x2="+4*2)
+document.write("<br><br>")
+document.write("4x3="+4*3)
+document.write("<br><br>")
+document.write("4x4="+4*4)
+document.write("<br><br>")
+document.write("4x5="+4*5)
+document.write("<br><br>")
+document.write("4x6="+4*6)
+document.write("<br><br>")
+document.write("4x7="+4*7)
+document.write("<br><br>")
+document.write("4x8="+4*8)
+document.write("<br><br>")
+document.write("4x9="+4*9)
+document.write("<br><br>")
+document.write("4x10="+4*10)
+document.write("<br><br>")
+document.write("Question no 3")
+document.write("<br><br>")
+document.write("Initial value"+" "+":"+" "+ini)
+document.write("<br><br>")
+document.write("<h2>Shopping Cart</h2>")
+document.write("The price of item 1 is"+" "+item1)
+document.write("<br><br>")
+document.write("Quantity of item 1 is"+" "+quantity1)
+document.write("<br><br>")
+document.write("The price of item 2 is"+" "+item2)
+document.write("<br><br>")
+document.write("Quantity of item 2 is"+" "+quantity2)
+document.write("<br><br>")
+document.write("Shipping Charges"+" "+ship)
+document.write("<br><br>")
+document.write("Total cost of your order is"+" "+totalc)
+document.write("<br><br>")
+document.write("<h1>Marks</h1>")
+document.write("Total Marks ="+" "+tmarks)
+document.write("<br><br>")
+document.write("Obtained Marks ="+" "+omarks)
+document.write("<br><br>")
+document.write("Percentage ="+" "+per)
+document.write("<br><br>")
+document.write("Currency in PKR")
+document.write("<br><br>")
+document.write("Total Currency in PKR is"+" "+pkr)
+document.write("<br><br>")
+document.write("<h2>Sequence</h2>")
+document.write("Sequence is"+" "+sequence)
+document.write("<br><br>")
+document.write("<h2>Age Calculator</h2>")
+document.write("Current Year :"+" "+cyear)
+document.write("<br><br>")
+document.write("Birth Year :"+" "+byear)
+document.write("<br><br>")
+document.write("Your Age :"+" "+age)
+document.write("<br><br>")
+document.write("<h2>The Geometrizer</h2>")
+document.write("The radius of a circle is"+" "+radius)
+document.write("<br><br>")
+document.write("The Circumstance is"+" "+circum)
+document.write("<br><br>")
+document.write("The area is"+" "+area)
+document.write("<br><br>")
+document.write("<h2>The Lifetime Supply Calculator</h2>")
+document.write("Favourite snack"+" "+snack)
+document.write("<br><br>")
+document.write("Current age :"+" "+cage)
+document.write("<br><br>")
+document.write("Estimated maximum age :"+" "+max)
+document.write("<br><br>")
+document.write("Amount of snack per day :"+" "+day)
+document.write("<br><br>")
+document.write("You will need"+" "+ tsnack +" "+snack+" "+"to last you untill the ripe age of" +" "+max)
